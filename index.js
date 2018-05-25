49438da12c08c4aef277b69c8752561a88d8326e
thaborak <thaborak@gmail.com>
2018-05-25 13:39:56 -0400

    delete endpoint and mock data

--- index.js
+++ index.js
@@ -13,6 +13,96 @@ function initSetup() {
       age: 2,
       breed: 'Bengal',
       story: 'Thrown on the street'
+    },
+    {
+      imageURL:'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Cute Cat',
+      name: 'Whiskers',
+      gender: 'Female',
+      age: 20,
+      breed: 'MaineCoon',
+      story: 'Abandoned by previous owner'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Cute Cat',
+      name: 'Tom',
+      gender: 'Female',
+      age: 6,
+      breed: 'Persian',
+      story: 'Abandoned by previous owner'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Exotic Cat',
+      name: 'Fluffy',
+      gender: 'Male',
+      age: 8,
+      breed: 'AmericanShorthair',
+      story: 'Owner moved to another country'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Exotic Cat',
+      name: 'Fluffy',
+      gender: 'Male',
+      age: 12,
+      breed: 'Persian',
+      story: 'Owner Died'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Loving Cat',
+      name: 'Baby',
+      gender: 'Male',
+      age: 0,
+      breed: 'tabby',
+      story: 'Stray'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Beautiful Coat',
+      name: 'MrKitty',
+      gender: 'Male',
+      age: 12,
+      breed: 'Balinese',
+      story: 'Thrown on the street'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Playful Cat',
+      name: 'Whiskers',
+      gender: 'Male',
+      age: 3,
+      breed: 'AmericanShorthair',
+      story: 'Stray'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Loving Cat',
+      name: 'Max',
+      gender: 'Female',
+      age: 19,
+      breed: 'bengal',
+      story: 'Owner moved to another country'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Cute Cat',
+      name: 'Tigger',
+      gender: 'Female',
+      age: 11,
+      breed: 'bengal',
+      story: 'Abandoned by previous owner'
+    },
+    {
+      imageURL: 'http://thecatapi.com/api/images/get?api_key=[MzE3Mjk3]&format=src&results_per_page=1',
+      imageDescription: 'Beautiful Coat',
+      name: 'Coco',
+      gender: 'Female',
+      age: 19,
+      breed: 'MaineCoon',
+      story: 'Thrown on the street'
     }
   ]);
   dogs.enqueue(
@@ -25,6 +115,96 @@ function initSetup() {
         age: 3,
         breed: 'Golden Retriever',
         story: 'Owner Passed away'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Good Boy',
+        name: 'Hope',
+        gender: 'Female',
+        age: 12,
+        breed: 'Pitbull',
+        story: 'Thrown on the street'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Great to have with kids',
+        name: 'Bailey',
+        gender: 'Male',
+        age: 10,
+        breed: 'Great Dane',
+        story: 'Owner Died'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Brown coat straight hair',
+        name: 'Morgan',
+        gender: 'Male',
+        age: 9,
+        breed: 'Poodle',
+        story: 'Thrown on the street'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Tall build with short black coat',
+        name: 'Scooby',
+        gender: 'Female',
+        age: 17,
+        breed: 'German Shepard',
+        story: 'Thrown on the street'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Brown coat straight hair',
+        name: 'Morgan',
+        gender: 'Male',
+        age: 2,
+        breed: 'German Shepard',
+        story: 'Thrown on the street'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Brown coat straight hair',
+        name: 'Buddy',
+        gender: 'Female',
+        age: 3,
+        breed: 'Great Dane',
+        story: 'Thrown on the street'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Curly blonde coat',
+        name: 'Charlie',
+        gender: 'Female',
+        age: 7,
+        breed: 'Bulldog',
+        story: 'Owner Died'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Good Boy',
+        name: 'Rover',
+        gender: 'Male',
+        age: 20,
+        breed: 'Husky',
+        story: 'Owner Died'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Great to have with kids',
+        name: 'Charlie',
+        gender: 'Male',
+        age: 14,
+        breed: 'Bulldog',
+        story: 'Owner moved to another country'
+      },
+      {
+        imageURL: 'https://dog.ceo/api/breeds/image/random',
+        imageDescription: 'Tall build with short black coat',
+        name: 'Bailey',
+        gender: 'Female',
+        age: 0,
+        breed: 'Golden Retreiver',
+        story: 'Thrown on the street'
       }
     ]);
 }
@@ -39,13 +219,21 @@ const { PORT, CLIENT_ORIGIN } = require('./config');
 
 const app = express();
 
-
+/* ========GET ENDPOINTS ========== */
 app.get('/api/cats', (req, res) => {
   res.json(peek(cats));
 });
 
 app.get('/api/dogs', (req, res) => {
   res.json(peek(dogs));
+});
+/* ========DELETE ENDPOINTS ========== */
+app.delete('/api/cats', (req, res) => {
+  res.cats.dequeue();
+});
+
+app.delete('/api/dogs', (req, res) => {
+  res.dogs.dequeue();
 });
 
 app.use(
